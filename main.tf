213f2a8387097a983bc3225f757694bfb112d5f1
ani50 <aniket.itapp@gmail.com>
2019-12-24 00:47:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 //--------------------------------------------------------------------
 // Modules
+variable "x" {}
+provider "azurerm" {
+  client_id = "${var.x}"
+}
 module "ani" {
   source  = "app.terraform.io/cmporg/ani/azure"
   version = "1.0.1"
