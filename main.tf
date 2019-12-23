2d02d21bc39d1fd9ae79fb7b7af4d7e3f252e9cf
ani50 <aniket.itapp@gmail.com>
2019-12-23 23:14:15 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+//--------------------------------------------------------------------
+// Modules
+module "ani" {
+  source  = "app.terraform.io/cmporg/ani/azure"
+  version = "1.0.1"
+
+  resource_group_name = "anitest"
+}
